dca2086b499b2a1bc4f34e59e1bf4ac42284607f
anu-oswal <anuoswal200299@gmail.com>
2023-12-06 20:24:05 +0100

    pca gtex result

--- pca.py
+++ pca.py
@@ -16,6 +16,7 @@ import IsoDatasets
 from torch import nn, Tensor
 import torch.optim as optim
 from sklearn.manifold import TSNE
+from sklearn.decomposition import IncrementalPCA
 from sklearn.decomposition import PCA
 from torch.nn.functional import softplus
 from torch.distributions import Distribution
@@ -25,53 +26,78 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from functools import reduce
 from collections import defaultdict
 
-# Define the train sets
-train_batch_size = 64
-archs4_train = IsoDatasets.Archs4GeneExpressionDataset("/dtu-compute/datasets/iso_02456/hdf5/")
-archs4_train_dataloader = DataLoader(archs4_train, batch_size=train_batch_size, shuffle=True)
-print("Archs4 training set size:", len(archs4_train))
+import h5py
+from tqdm import tqdm
 
-# Define the test sets (gtex_gene_expression)
-eval_batch_size = 64
-gtex_test = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5/", include='brain')
-gtex_test_dataloader = DataLoader(gtex_test, batch_size=eval_batch_size, shuffle=True)
-print("Gtex test set size:", len(gtex_test))
 
-def apply_pca_on_dataloader(dataloader, n_components=2):
-    # Extract gene expression data from batches
-    all_data = []
-    for batch in dataloader:
-        gene_expression_data = batch.cpu().numpy()  # Assuming your data is in numpy format
-        all_data.append(gene_expression_data)
 
-    # Combine data from all batches
-    combined_data = np.concatenate(all_data, axis=0)
+n_components = 12
+eval_batch_size = 64 
+gtex_test = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5/") 
+gtex_test_dataloader = DataLoader(gtex_test, batch_size=eval_batch_size, shuffle=True) 
+ipca = IncrementalPCA(n_components=n_components)
+print(type(gtex_test_dataloader)) 
+# Print the first 5 batches of the DataLoader
+for batch_index, (inputs, labels) in enumerate(gtex_test_dataloader):
+    if batch_index < 5:
+        print(f"Batch {batch_index + 1}:")
+        print("Inputs:")
+        print(inputs)
+        print("Labels:")
+        print(labels)
+       
+    else:
+        break
+# Accumulate data for PCA
+all_data = []
+for batch_index, (inputs, _) in enumerate(gtex_test_dataloader):
+    all_data.append(inputs.numpy())
 
-    # Standardize the data
-   # scaler = StandardScaler()
-   # standardized_data = scaler.fit_transform(combined_data)
+# Concatenate data and fit PCA
+all_data = np.concatenate(all_data, axis=0)
+ipca.fit(all_data)
 
-    # Perform PCA
-    pca = PCA(n_components=n_components)
-    principal_components = pca.fit_transform(combined_data)
+# Transform data with IPCA
+transformed_data = ipca.transform(all_data)
 
-    return principal_components
-
-# Apply PCA on the training DataLoader
-archs4_pca = apply_pca_on_dataloader(archs4_train_dataloader)
-
-# Apply PCA on the test DataLoader
-gtex_pca = apply_pca_on_dataloader(gtex_test_dataloader)
-
-# Visualize Results
-plt.figure(figsize=(10, 6))
-plt.scatter(archs4_pca[:, 0], archs4_pca[:, 1], label='Archs4 Training Data')
-plt.scatter(gtex_pca[:, 0], gtex_pca[:, 1], label='Gtex Test Data')
-plt.title('PCA')
+# Plot the first two principal components
+plt.scatter(transformed_data[:, 0], transformed_data[:, 1], alpha=0.5)
+plt.title('Principal Components of Gene Expression Data')
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
-plt.legend()
 plt.show()
+plt.savefig("PCA_gtex.png")
+#all_principal_components_gtex = []
+# Iterate over batches in the dataloader and update the principal components
+#for X in tqdm(gtex_test_dataloader):
 
 
-plt.savefig('pca.png')
+    # Convert PyTorch tensors to NumPy arrays
+ #   X_numpy = [tensor.numpy() for tensor in X]
+
+    # Find the maximum length of sequences
+  #  max_length = max(len(seq) for seq in X_numpy)
+
+    # Pad sequences to the maximum length
+   # X_padded = [np.concatenate([seq, np.zeros(max_length - len(seq))]) for seq in X_numpy]
+
+    # Convert to NumPy array
+    #X_array = np.array(X_padded)
+     
+    #ipca.partial_fit(X_array)
+    #principal_components = ipca.transform(X.numpy())
+    #all_principal_components_gtex.append(principal_components)
+#all_principal_components_gtex = np.concatenate(all_principal_components_gtex, axis=0)
+#print(all_principal_components_gtex)
+# Plotting the first two principal components
+#plt.scatter(all_principal_components_gtex[:, 0], all_principal_components_gtex[:, 1], alpha=0.5)
+#plt.title('Principal Components of Gene Expression Data')
+#plt.xlabel('Principal Component 1')
+#plt.ylabel('Principal Component 2')
+#plt.show()
+#plt.savefig("PCA_gtex.png")
+
+#output_file_path = "principal_components_gtex.h5"
+#with h5py.File(output_file_path, 'w') as hf:
+ #   hf.create_dataset("principal_components_gtex", data=all_principal_components_gtex)
+
