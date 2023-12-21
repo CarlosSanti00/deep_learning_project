0d2a736342544a119a187a2b09f787dde355f3a6
CarlosSanti00 <98809634+CarlosSanti00@users.noreply.github.com>
2023-12-21 14:56:02 +0100

    Update FFNN_Final.py

--- Project29_scripts/FFNN_Final.py
+++ Project29_scripts/FFNN_Final.py
@@ -34,6 +34,27 @@ class RegressionModel(nn.Module):
         x = self.fc2(x)
         return x
 
+# Feed-forward Neural network architecture (original input size)
+class RegressionModel_complete(nn.Module):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size, dropout_rate=0):
+        super(RegressionModel_complete, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size_1)
+        self.relu1 = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc2 = nn.Linear(hidden_size_1, hidden_size_2)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size_2, output_size)
+
+    # Forward pass
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        return x
+
 # Class for reading GTEX dataset (including tissues labels for stratification of the dataset in CV folds)
 class GtexExpressionDataset(torch.utils.data.Dataset):
     def __init__(self, data_dir: str, include: str = "", exclude: str = "", load_in_mem: bool = False, col_names: list = None, VAE: bool = False):
@@ -130,7 +151,7 @@ for fold, (train_indices, test_indices) in enumerate(stratified_kfold.split(np.z
 
     # Instantiate the model with dropout
     model = RegressionModel(input_size, n_hidden, output_size, dropout_rate=0)
-    model_og = RegressionModel(input_size_og, n_hidden, output_size, dropout_rate=0)
+    model_og = RegressionModel_complete(input_size_og, 256, 32, output_size, 0)
 
     # Define loss function and optimizer
     criterion = nn.MSELoss()
